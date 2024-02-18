a7fa1655ece1d01ac8ad76f283e350a30193888c
Bogdan Nebylytsia <bohdan.deal.ua@gmail.com>
2024-02-18 10:17:34 +0200

    Animating a RandomChar Component

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -3,13 +3,14 @@ import useMarvelService from '../../services/MarvelSevice';
 import ErrorMassage from '../errorMassage/ErrorMassage';
 import Spinner from '../spinner/Spinner';
 import mjolnir from '../../resources/img/mjolnir.png';
+import { CSSTransition, SwitchTransition } from 'react-transition-group';
 import './randomChar.scss';
 
 const RandomChar = () => {
 
    const [char, setChar] = useState({});
 
-   const {loading, error, clearError, getCharacter} = useMarvelService();
+   const { loading, error, clearError, getCharacter } = useMarvelService();
 
    useEffect(() => {
       updateChar();
@@ -25,42 +26,45 @@ const RandomChar = () => {
       const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
 
       getCharacter(id)
-      .then(onCharLoaded)
+         .then(onCharLoaded)
    }
 
-      const errorMassage = error ? <ErrorMassage /> : null; 
-      const spinner = loading ? <Spinner /> : null;
-      const content = !(error || loading || !char) ? <RandomCharBlock char={char} /> : null;
+   return (
+      <div className="randomchar">
 
-      return (
-         <div className="randomchar">
-            
-            {errorMassage}
-            {spinner}
-            {content}
+         <SwitchTransition>
+            <CSSTransition
+               key={!(error || loading || !char) ? "content" : loading ? "spinner" : "error"}
+               timeout={450}
+               classNames={"fade"}>
+               {!(error || loading || !char) ? <RandomCharBlock char={char} /> : loading ? <Spinner /> : <ErrorMassage />}
+            </CSSTransition>
+         </SwitchTransition>
 
-            <div className="randomchar__static">
-               <p className="randomchar__title">
-                  Random character for today! <br />
-                  Do you want to get to know him better?
-               </p>
-               <p className="randomchar__title">
-                  Or choose another one
-               </p>
-               <button className="button button__main" onClick={updateChar}>
-                  <div className="inner">try it</div>
-               </button>
-               <img src={mjolnir} alt="mjolnir" className="randomchar__decoration" />
-            </div>
+
+
+         <div className="randomchar__static">
+            <p className="randomchar__title">
+               Random character for today! <br />
+               Do you want to get to know him better?
+            </p>
+            <p className="randomchar__title">
+               Or choose another one
+            </p>
+            <button className="button button__main" onClick={updateChar}>
+               <div className="inner">try it</div>
+            </button>
+            <img src={mjolnir} alt="mjolnir" className="randomchar__decoration" />
          </div>
-      )
+      </div>
+   )
 
 }
 
 const RandomCharBlock = ({ char }) => {
 
    const { name, description, thumbnail, homepage, wiki } = char;
-  
+
    let imgClass = "randomchar__img";
    if (thumbnail === "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg") {
       imgClass = "randomchar__img-not-available";
@@ -69,7 +73,7 @@ const RandomCharBlock = ({ char }) => {
       <div className="randomchar__block">
          <img src={thumbnail} alt="Random character" className={imgClass} />
          <div className="randomchar__info">
-            <p className="randomchar__name">{name && name.length > 20 ? `${char.name.substring(0,20)}...` : name}</p>
+            <p className="randomchar__name">{name && name.length > 20 ? `${char.name.substring(0, 20)}...` : name}</p>
             <p className="randomchar__descr">
                {description}
             </p>
